1b012e74997680f3ed301be19bb4e13ef6bfd641
Gerardo Castañeda <zl1fmercury@gmail.com>
2022-12-27 00:53:22 -0600

    update client

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,47 +1,59 @@
-import React from 'react'
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useContext } from 'react'
-import { AuthContext } from '../context/authContext'
+import React, { useState } from "react";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 
 const Login = () => {
   const [inputs, setInputs] = useState({
     username: "",
     password: "",
-  })
-  const [err, setError] = useState(null)
+  });
+  const [err, setError] = useState(null);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const { login } = useContext(AuthContext)
-  
-  const handleChange = e => {
-    setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
-  }
+  const { login } = useContext(AuthContext);
 
-  const handleSubmit = async e => {
-    e.preventDefault()
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
       await login(inputs)
-      navigate("/")
+      navigate("/");
     } catch (err) {
-      setError(err.response.data)
+      setError(err.response.data);
     }
-  }
-  
+  };
   return (
-    <div className='auth'>
+    <div className="auth">
       <h1>Login</h1>
-      <form action="">
-        <input required type="text" placeholder='username' name='username' onChange={handleChange} />
-        <input required type="password" placeholder='password' name='password' onChange={handleChange} />
+      <form>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
         <button onClick={handleSubmit}>Login</button>
         {err && <p>{err}</p>}
-        <span>Don't have an account? <Link to="/register">Register</Link>
+        <span>
+          Don't have an account? <Link to="/register">Register</Link>
         </span>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,41 +1,83 @@
-import React from 'react'
-import Edit from '../img/edit.png'
-import Delete from '../img/delete.png'
-import { Link } from 'react-router-dom'
-import Menu from '../components/Menu'
+import React, { useEffect, useState } from "react";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { useContext } from "react";
+import { AuthContext } from "../context/authContext";
+import DOMPurify from "dompurify";
 
 const Single = () => {
-  return (
-    <div className='single'>
-      <div className='content'>
-        <img 
-          src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="post cover"
-        />
-        <div className="user">
-          <img 
-            src="https://images.pexels.com/photos/6964334/pexels-photo-6964334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="profile portrait" 
-          />
-          <div className="info">
-            <span>John</span>
-            <p>Posted 2 days ago</p>
-          </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img src={Delete} alt="" />
-          </div>
-        </div>
-        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum?</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos ducimus, eos inventore consequatur, facere cumque ipsa sed labore velit perspiciatis excepturi tempora est quidem quae quo? Amet molestiae esse est sint sequi ea dignissimos aperiam odit dolorum placeat deserunt laboriosam quam veniam alias, ullam ad. Modi dolore tempora ratione impedit!</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae eveniet quo debitis, obcaecati nihil voluptas asperiores quidem sunt laboriosam? Alias obcaecati id atque amet odit, autem cum doloremque nobis laboriosam voluptatem quaerat. Architecto blanditiis est odit nisi esse neque possimus aut hic accusamus quod odio in error, exercitationem explicabo mollitia.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates voluptatem commodi nisi, nostrum debitis quaerat nihil dolor quasi excepturi fugiat consequatur fuga facere eveniet atque, maxime quidem iusto. Asperiores, sapiente provident? At vel quod ex nisi velit eos, a voluptate nemo, sint ratione rerum assumenda autem eum exercitationem nihil officia?</p>
-      </div>
-      <Menu />
-    </div>
-  )
-}
+  const [post, setPost] = useState({});
 
-export default Single
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const postId = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
+  const handleDelete = async ()=>{
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/")
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  /* const getText = (html) =>{
+    const doc = new DOMParser().parseFromString(html, "text/html")
+    return doc.body.textContent
+  } */
+
+  return (
+    <div className="single">
+      <div className="content">
+        <img src={`../upload/${post?.img}`} alt="" />
+        <div className="user">
+          {post.userImg && <img
+            src={post.userImg}
+            alt=""
+          />}
+          <div className="info">
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
+          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`} state={post}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt="" />
+            </div>
+          )}
+        </div>
+        <h1>{post.title}</h1>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(post.desc),
+          }}
+        >
+        </p>      
+      </div>
+      <Menu cat={post.cat}/>
+    </div>
+  );
+};
+
+export default Single;
